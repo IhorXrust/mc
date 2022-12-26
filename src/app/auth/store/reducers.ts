d053b3dbb5cd5a3f953d75e7a8f533cd1bef258e
Ihor <i.khrushchevskii@gmail.com>
2022-12-26 18:55:04 +0100

    refresh auth reducer, refresh auth actions, add backend errors interface

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,9 +1,16 @@
 import { createReducer, on } from '@ngrx/store';
 import { AuthStateInterface } from '../types/authState.interface';
-import { registerAction } from './actions/register.action';
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from './actions/register.action';
 
 const initState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  isLoggeIn: null,
+  validationErrors: null,
 };
 
 export const authReducer = createReducer(
@@ -13,6 +20,20 @@ export const authReducer = createReducer(
     (state): AuthStateInterface => ({
       ...state,
       isSubmitting: true,
+      validationErrors: null,
     })
-  )
+  ),
+
+  on(registerSuccessAction, (state, action) => ({
+    ...state,
+    isSubmitting: false,
+    currentUser: action.currentUser,
+    isLoggeIn: true,
+  })),
+
+  on(registerFailureAction, (state, action) => ({
+    ...state,
+    isSubmitting: false,
+    validationErrors: action.errors,
+  }))
 );

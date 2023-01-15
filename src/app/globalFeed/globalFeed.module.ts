7891027ab90b17acf930dee24ef11f0c6eddee6e
Ihor <i.khrushchevskii@gmail.com>
2023-01-15 22:28:43 +0100

    add feed toggler

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BannerModule } from '../shared/modules/baner/baner.module';
 import { FeedModule } from '../shared/modules/feed/feed.module';
+import { FeedToggleModule } from '../shared/modules/feedToggle/feedToggle.module';
 import { PopularTagsModule } from '../shared/modules/popularTags/popularTags.module';
 import { GlobalFeedComponent } from './components/globalFeed/globalFeed.component';
 
@@ -20,6 +21,7 @@ const routes = [
     FeedModule,
     BannerModule,
     PopularTagsModule,
+    FeedToggleModule,
   ],
   declarations: [GlobalFeedComponent],
 })
